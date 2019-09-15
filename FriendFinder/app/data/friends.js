794a42fc578addebecbc35bba65551d4b27b7a4a
Jeffrey Davis <davisj0214@yahoo.com>
2019-09-15 00:16:09 -0500

    app functionality and styling complete

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -13,39 +13,39 @@ var friends = [
       2,
       5,
       4,
-      1
+      1 // 32
     ]
   },
   {
     name: "Rick",
     photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/6/68/Screenshot_2015-09-24_at_1.41.41_PM.png/revision/latest?cb=20150924174425",
     scores: [
-      3,
-      3,
-      3,
-      3,
-      3,
-      3,
-      3,
-      3,
-      3,
-      3
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1 // 10
     ]
   },
   {
     name: "Jerry",
     photo: "https://vignette.wikia.nocookie.net/ricksanchez/images/f/f6/Jerry_Smith.jpg/revision/latest?cb=20160609043244",
     scores: [
-      2,
       4,
-      1,
-      2,
-      3,
       4,
-      5,
-      5,
-      3,
-      1
+      4,
+      4,
+      4,
+      4,
+      4,
+      4,
+      4,
+      4 // 40
     ]
   },
   {
@@ -61,56 +61,24 @@ var friends = [
       5,
       5,
       5,
-      5
+      5 // 50
     ]
   },
   {
-    name: "Mr. Poopybutthole",
-    photo: "https://i1.sndcdn.com/artworks-000131941579-k9t4qc-t500x500.jpg",
-    scores: [
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1
-    ]
-  },
-  {
-    name: "Mr. Meeseeks",
-    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFBPqoS2sVFdcMG4wjvrHfyhnwBovlI8S3bHYHZNzZNogsOGp5",
-    scores: [
-      2,
-      2,
-      2,
-      4,
-      4,
-      4,
-      1,
-      2,
-      3,
-      3
-    ]
-  },
-{
-  name: "Summer",
+    name: "Summer",
     photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/ad/Summer_is_cool.jpeg/revision/latest?cb=20160919183158",
     scores: [
-      1,
       2,
-      3,
-      4,
-      5,
-      5,
-      4,
-      3,
       2,
-      1
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2,
+      2 // 20
     ]
-}]
+  }]
 
 module.exports = friends;
